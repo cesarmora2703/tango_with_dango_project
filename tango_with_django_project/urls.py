7b0e56de42c4440ed0827188c609de3730709dcd
cesarmora2703 <cesarmora2703@gmail.com>
2023-05-02 11:48:56 -0500

    Change urls for all project including accounts/ path.

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import include
 
-#Neede for emdia files
+# Neede for emdia files
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -28,4 +28,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('rango/', include('rango.urls')),
     path('admin/', admin.site.urls),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('accounts/', include('registration.backends.simple.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
